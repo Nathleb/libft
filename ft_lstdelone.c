b850cd87ad522883cd882a90acb1250db82467c2
Lebibi <lebihannathan@gmail.com>
2020-11-26 17:58:05 +0100

    ????

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,13 +6,13 @@
 /*   By: nle-biha <nle-biha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 17:07:22 by nle-biha          #+#    #+#             */
-/*   Updated: 2020/11/26 17:26:34 by nle-biha         ###   ########.fr       */
+/*   Updated: 2020/11/26 17:57:54 by nle-biha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstdelone(t_list *lst, void (*del)(void*))
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
 	if (lst)
 	{
