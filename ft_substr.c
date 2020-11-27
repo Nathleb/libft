340799cf77122d644bff19b7dd37b34130f08662
Lebibi <lebihannathan@gmail.com>
2020-11-27 19:36:44 +0100

    substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: nle-biha <nle-biha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 22:22:40 by nle-biha          #+#    #+#             */
-/*   Updated: 2020/11/26 21:44:36 by nle-biha         ###   ########.fr       */
+/*   Updated: 2020/11/27 19:36:27 by nle-biha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if ((ret = (char *)malloc(len + 1)) == NULL)
 		return (NULL);
 	if (start >= ft_strlen(s))
+	{
+		ret[0] = '\0';
 		return (ret);
+	}
 	while (i < len && s[start + i])
 	{
 		ret[i] = s[start + i];
